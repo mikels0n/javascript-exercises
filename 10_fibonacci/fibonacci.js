4221fdccaa8bb661df97f856451ef3743e6c2f25
mikels0n <myskamichal@gmail.com>
2024-02-29 18:36:22 +0100

    fibonacci solution added

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
-
+const fibonacci = function(member) {
+    const arr = [];
+    if (member < 0) {
+        return("OOPS");
+    }
+    for (let i = 1; i <= member; i++) {
+        if (i === 1) {
+            arr.push(1);
+        }
+        else if (i === 2) {
+            arr.push(1);
+        }
+        else {
+            let a = arr[(i - 3)];
+            let b = arr[(i - 2)];
+            arr.push(a + b);
+        }
+    }
+    return(arr[arr.length - 1]);
 };
 
 // Do not edit below this line
