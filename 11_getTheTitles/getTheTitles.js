107a4d758ff3e2a8dfabced294ab329c56fc7569
mikels0n <myskamichal@gmail.com>
2024-02-29 19:05:56 +0100

    added getTheTitles solution

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,11 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(arr) {
+    let counter = arr.length - 1;
+    const titles = [];
+    for (let i = 0; i <= counter; i++) {
+        let obj = arr[i];
+        titles.push(obj.title);
+    };
+    return(titles);
 };
 
 // Do not edit below this line
